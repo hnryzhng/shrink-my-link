3e0d9f71b182787ae3c60f38a54313dfdb9bfb06
Henry <hnryzhng@gmail.com>
2019-11-27 23:12:22 -0600

    work on main route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
+const Urls = require(".models/urls.js")
+
 // INSTANTIATE APP 
 const app = express();
 const router = express.Router();
@@ -20,27 +22,52 @@ mongoose
 	.connect(
 		dbRoute,
 		{	useNewUrlParser: true,
-			useUnifiedTopology: true	// new mongoose version requires this parameter
+			useUnifiedTopology: true	// this mongoose version requires this parameter
 		},
 	)
 	.then(() => console.log("connected to MongoDB database"))
 	.catch((err) => console.log("error connecting to MongoDB:", err));
 
 
-/**
-MAIN CODE BLOCK FOR ROUTES
+
+// ROUTES
 
 // main route: 
 // check if url is already in db upon sending request
 // if not, shorten url, insert in db, then send response obj with shortened url
 // else, retrieve shortened url from db and send response obj with shortened url 
+router.post("/shrink", (req, res) => {
+	const longUrl = req.body.longUrl;	// front-end param should be longUrl
+	console.log("long url:", longUrl);
+
+	Urls.connect({ long-url: longUrl }).then((urlDoc) => {
+		if (!urlDoc) {
+
+			// if not found
+			// shorten url with module
+			// insert long url and shortened url as a doc in the db
+			// send response obj with shortened url
+
+			// alternative
+			// console.log("url doc not found in db");
+			// res.status(400).json({ error: "url not found" });
+			// return
+		} else {
+			// if found
+
+		}
 
 
-//
 
 
-**/
+	});
+
+
+
+})
+
+
+
 
 app.use("/api", router);	// allows react to send api request using "api" in request URL
-
 app.listen(api_port, () => console.log(`Listening to ${api_port}`));
