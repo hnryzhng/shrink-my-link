abce2edc46371860a87b679cc2f35a954c6c437b
Henry <hnryzhng@gmail.com>
2019-11-30 12:16:18 -0600

    basic front-end working

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,22 @@ class App extends Component {
 
   state = {
   	urlInput: null,
-    links: [] // array of pairs/objects of long and short urls
+    links: [{long_url: "lol", short_url: "haha"}] // array of pairs/objects of long and short urls
   }
 
   render() {
     
     return (
+
       <div>
-        <input type= "text" style={{ width: "300px" }} placeholder="Shrink me!" name="originalUrl" onChange={{ event => this.setState({ urlInput: event.target.value }) }}>
+        <h1> SHRINK MY LINK </h1>
+
+        <input type="text" style={{ width: "300px" }} placeholder="Shrink me!" name="originalUrl" onChange={ event => this.setState({ urlInput: event.target.value }) } />
 
 
         <div id="listContainer" style={{ width: "300px", height: "500px", border: "1px solid black" }}>
           <List links={ this.state.links } />
         </div>
-
       </div>
     );
   }
@@ -29,6 +31,7 @@ class List extends Component {
 
   render() {
     let list = this.props.links.map((urlObj, index) => {
+      console.log("index, urlObj:", index, urlObj)
       return <Item key={index} urlObj={urlObj} />
     });
 
@@ -42,12 +45,11 @@ class Item extends Component {
 
   render() {
 
-    const i = this.props.key;
     const urlObj = this.props.urlObj;
 
     return(
       <div>
-        <li id={ i } long_url={{ urlObj.long_url }} short_url={{ urlObj.short_url }} />
+        <li long_url={ urlObj.long_url } short_url={ urlObj.short_url } ></li>    
       </div>
     )
 
