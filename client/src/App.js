3bfffdf1e79e9feb451ae15d230f49a75187c701
Henry <hnryzhng@gmail.com>
2019-11-30 11:52:00 -0600

    work on front-end

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,55 @@ import axios from 'axios';
 class App extends Component {
 
   state = {
-  	user: null
+  	urlInput: null,
+    links: [] // array of pairs/objects of long and short urls
   }
 
   render() {
     
     return (
       <div>
-      	<p>Hello World</p>
+        <input type= "text" style={{ width: "300px" }} placeholder="Shrink me!" name="originalUrl" onChange={{ event => this.setState({ urlInput: event.target.value }) }}>
+
+
+        <div id="listContainer" style={{ width: "300px", height: "500px", border: "1px solid black" }}>
+          <List links={ this.state.links } />
+        </div>
+
       </div>
     );
   }
 
 }
 
+class List extends Component {
+
+  render() {
+    let list = this.props.links.map((urlObj, index) => {
+      return <Item key={index} urlObj={urlObj} />
+    });
+
+    return <ul> {list} </ul>
+
+  }
+
+}
+
+class Item extends Component {
+
+  render() {
+
+    const i = this.props.key;
+    const urlObj = this.props.urlObj;
+
+    return(
+      <div>
+        <li id={ i } long_url={{ urlObj.long_url }} short_url={{ urlObj.short_url }} />
+      </div>
+    )
+
+
+  }
+
+}
 export default App;
