71e3f0db245289f931c393884a519162d3ee0e00
Henry <hnryzhng@gmail.com>
2019-12-10 17:18:51 -0600

    updated tests

--- test/test-simple.js
+++ test/test-simple.js
@@ -4,7 +4,6 @@ const should = chai.should();
 
 const server = require('../backend/server.js');
 
-// call external packages
 const Shrinker = require('../backend/shrinker.js');
 
 // call Mongoose models
@@ -14,26 +13,34 @@ const Urls = require('../backend/models/urls.js')
 // call chai-http middleware
 chai.use(chaiHttp);
 
-// tests 
+// tests
 
 describe('test shrinking of url', function(done) {
 	// route: /api/shrink
 
 	const longUrl = "";
 	const uniqueCounter = 0;
+	const shortUrl = Shrinker.shrink(uniqueCounter);	// create shortened url based on test counter using Shrinker.js
 
 	before(function(done){
 		
-		// connect to database
+		// connect to database?
+
+		// create dummy url doc with keys {doc_id, long_url, short_url}
+		const testUrlDoc = new Url({
+			doc_id: uniqueCounter,
+			long_url: longUrl,
+			short_url: shortUrl
+		});
 
 		// create dummy counter doc to put in Counter collection
 		const testCounterDoc = new Counter({
 			unique_counter: uniqueCounter
 		});
 
+		// insert dummy docs into respective collections
+		testUrlDoc.save();
 		testCounterDoc.save();
-		
-		// then put dummy doc with keys {doc_id, long_url, short_url} in Url collection
 
 	});
 
@@ -41,36 +48,57 @@ describe('test shrinking of url', function(done) {
 		// remove counter and url dummy docs from collection (be careful to not dump entire collection...)
 	})
 
-	it('should return expected shortened url given long url', function(done) {
+
+	it('should return expected shortened url given long test url', function(done) {
 
 		// given long url, shrink it using Shrinker.js
-		const actualShortUrl = Shrinker.shrink(uniqueCounter);
+		const actualShortUrl = shortUrl;
 
 		// expected output should equal actual shortUrl
 		assert('', actualShortUrl);
 
+		done();
+
 	})
 
-	it('should return response object with shortened url given long test url', function(done) {
-			
-		// create shortened url based on test counter using Shrinker.js
-		// return response object { success: true, long_url: longUrlInput, short_url: targetShortUrl }
+
+	it('should return test counter doc', function(done) {
+
+		done();
+
+	});
+
+	it('should retrieve test url doc', function(done) {
 		
+		// retrieve Url record created in 'before' hook from db
+		
+		done();
+
+	})
+
+
+	it('route should return proper response object given long test url at /shrink GET', function(done) {
+		
+		// return response object { success: true, long_url: <long test url>, short_url: TYPEOF STRING }
+
+		// response should be json
+		// response should return status 200
+		// response object should have property 'success'
+		// response object should have property 'long_url'
+		// response object should have property 'short_url'
+		// value for 'success' property should be true
+		// value for 'long_url' should be longUrl
+		// value for 'short_url' should be shortUrl
+		// type for short url should be alphanumeric string
+		// short url should have length greater than 0
 
 		done();
 	});
 
-	it('should return test counter doc', function(done) {
-
-	});
-
-	it('should retrieve url test doc in Url collection', function(done) {
-		
-		// retrieve Url record created in 'before' hook from db
-		
-	})
-
 })
+
+
+
 
 
 describe('test redirect given long url');
